004b8eb2e44c95062d6c867b7e46b3ce8a6a22b7
Kenel Cruz <kenelcruz@gmail.com>
2021-08-18 00:07:35 -0400

    Post paciente 89%..

--- src/app/View/Paciente/add-paciente/add-paciente.component.ts
+++ src/app/View/Paciente/add-paciente/add-paciente.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup, ReactiveFormsModule  } from '@angular/forms';
+import {Router, ActivatedRoute} from '@angular/router';
+import {PacienteServiceService} from '../../../Services/PacienteService/paciente-service.service'
+import Swal from 'sweetalert2'
+import { IPacientes } from 'src/app/Models/Pacientes-interface';
 @Component({
   selector: 'app-add-paciente',
   templateUrl: './add-paciente.component.html',
@@ -7,9 +11,90 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPacienteComponent implements OnInit {
 
-  constructor() { }
+  nuevoForm = new FormGroup({
+    //paciente
+    paccienteId : new FormControl(''),
+    nombre: new FormControl(''),
+    apellido: new FormControl(''),
+    tipo_Documento  : new FormControl(''),
+    valor_Tipo_Contenido : new FormControl(''),
+    edad  : new FormControl(''),
+    fecha_Nacimiento : new FormControl(''),
+    nacionalidad    : new FormControl(''),
+    celular : new FormControl(''),
+    direccion : new FormControl(''),
+    motivoConsulta  : new FormControl(''),
+    alta  : new FormControl(''),
+
+//Medicamento
+    nombreM: new FormControl(''),
+    canitdad : new FormControl(''),
+    tipoM : new FormControl(''),
+    labiorio : new FormControl(''),
+    fechaExpiracion   : new FormControl(''),
+
+ //Enfermedad
+    nombreE : new FormControl(''),
+    tipoE  : new FormControl(''),
+    sintomas  : new FormControl(''),
+    nivel_Peligro     : new FormControl(''),
+
+
+    enfermedades_infantiles  : new FormControl(''),
+    secuelas_infanteles : new FormControl(''),
+    enfermedades_adolecencia   : new FormControl(''),
+    secuelas_adolecencia   : new FormControl(''),
+    enfermedades_Adultez  : new FormControl(''),
+    secuelas_Adultez : new FormControl(''),
+    hospitalizaciones   : new FormControl(''),
+    antecedentesQx  : new FormControl(''),
+    transfuciones   : new FormControl(''),
+    fracturas  : new FormControl(''),
+    ets     : new FormControl(''),
+    alergicos  : new FormControl(''),
+    espesificacion_alergica  : new FormControl(''),
+    tetano   : new FormControl(''),
+    difeeria   : new FormControl(''),
+    sarampion      : new FormControl(''),
+    quejarse   : new FormControl(''),
+    influenza   : new FormControl(''),
+    herpes    : new FormControl(''),
+    otros    : new FormControl(''),
+
+
+  });
+
+  constructor(private router: Router, private PacienteServiceService: PacienteServiceService) { }
 
   ngOnInit(): void {
   }
 
+  public postForm(form: IPacientes){
+
+    if(form!==null){
+    this.PacienteServiceService.postPaciente(form).subscribe((res: any)=>{
+
+
+      Swal.fire({
+
+        icon: 'success',
+        title: 'Registro exitoso',
+        showConfirmButton: false,
+        timer: 1500
+      })
+        this.router.navigate(['Pacientes'])
+
+
+    })
+      }if (form == null){
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Favor de rellenar los campos',
+        })
+
+        this.router.navigate(['/AddPaciente']);
+      }
+
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'
+import {DpDatePickerModule} from 'ng2-date-picker';
 import { AppRoutingModule, routinComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './Components/sidebar/sidebar.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { InfoCardComponent } from './Components/info-card/info-card.component';
 import { TablaPacienteComponent } from './Components/tabla-paciente/tabla-paciente.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 //Services
 import { PacienteServiceService } from './Services/PacienteService/paciente-service.service';
 import { UsuarioServiceService } from './Services/UsuarioService/usuario-service.service';
@@ -28,7 +30,10 @@ import { LoginComponent } from './View/login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    DpDatePickerModule,
+
+    ReactiveFormsModule
   ],
   providers: [PacienteServiceService, UsuarioServiceService],
   bootstrap: [AppComponent]
